fb53f62262237d3a7cc1917e9c9d63a00e45007b
TimurEG <54975875+TimurEG@users.noreply.github.com>
2020-03-26 18:34:21 +0300

    Update timurtest.dart

--- timurtest/lib/timurtest.dart
+++ timurtest/lib/timurtest.dart
@@ -1,3 +1,25 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:async';
+
+void main() async {
+  final a = await Stream.fromIterable(List<int>.generate(100, (e) => e + 1))
+      .map((e) => fizzBuzzshit(e))
+      .toList();
+  print(a);
+}
+
+
+
+String fizzBuzzshit(int s) {
+  if (s % 15 == 0) {
+    return 'FizzBuzz';
+    
+  } else if (s % 5 == 0) {
+    return 'Buzz';
+    
+  } else if (s % 3 == 0) {
+    return 'Fizz';
+    
+  } else {
+    return '$s';
+  }
 }
